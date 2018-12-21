68d93893a92c48ba3eb829dbed3b918ac3a2db31
Jordan Belinsky <jordanbelinsky1@gmail.com>
2018-12-20 22:59:41 -0800

    Test commit

--- educalculate.py
+++ educalculate.py
@@ -11,6 +11,8 @@ values = []
 avg_num = 0                     # empty integer to calculate the numerator of the weighted average calculation
 avg_den = 0                     # empty integer to calculate the denominator of the weighted average calculation
 
+# TEST #
+
 # states #
 intro = True
 num_check = False
